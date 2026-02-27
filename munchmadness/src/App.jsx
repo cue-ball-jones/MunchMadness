eeed6ce5a3dc8db4bbb4fcce41cb5485d2ca79be
Lex Graves <acg6810@gmail.com>
2026-02-26 21:01:33 -0600

    variables added + some general positioning

--- munchmadness/src/App.jsx
+++ munchmadness/src/App.jsx
@@ -4,31 +4,61 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const streakVal = "";
+  
+  const calGoal = "";
+  const proGoal = "";
+  const carbGoal = "";
+  const fatGoal = "";
+
+  const currCal = "";
+  const currPro = "";
+  const currCarb = "";
+  const currFat = "";
+
+
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div class="streak">
+          <p>Streak:</p>
+        </div>
+
+        <div>
+          <h1>Munch.org</h1>
+        </div>
+        
+        <div>
+          <p>Current Stats</p>
+          <p>Calories:</p>
+          <p>Protein:</p>
+          <p>Carbohydrates:</p>
+          <p>Fat:</p>
+        </div>
+
+        <div>
+          <p>Track Meal</p>
+
+          <div class="logContainer">
+         
+            <div>
+              <p>Calories:</p>
+              <input></input>
+              <p>Carbohydrates:</p>
+              <input></input>
+            </div>
+
+            <div>
+              <p>Protein:</p>
+              <input></input>
+              <p>Fat:</p>
+              <input></input>
+            </div>
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
